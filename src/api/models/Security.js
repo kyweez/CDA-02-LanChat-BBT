b37175f8cf47d4ed616e8ee82b4663e284f43dff
kyweez <auboudier.pro@gmail.com>
2020-07-16 15:28:14 +0200

    #14-Review / Migrate isUnbique Methodes + regex pwd

--- src/api/models/Security.js
+++ src/api/models/Security.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt');
 const regexName = /^([A-Za-z]{2,30})([ \-]{1})?([A-Za-z]{2,30})$/;
 const regexUser = /^([a-zA-Z0-9]{1,9})([a-zA-Z]{1,2})([a-zA-Z0-9]{1,9})$/;
 const regexEmail = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-const regexPassword = /^[0-9]{3,}$/;
+const regexPassword = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[#$%*+=])[A-Za-z\d#$%*+=]{8,}$/;
 
 class Security {
     static isValidString(_string, _regex) {
@@ -94,24 +94,6 @@ class Security {
          * @todo : Proteger le truc
          */
         return bcrypt.compareSync(_userInput, _passwordDB);
-    }
-
-    static isUniqueId(_userManager, _id) {
-        if ((_userManager.getUserTab().find(item => item.getId() === _id)) !== undefined)
-            return false;
-        return true;
-    }
-
-    static isUniqueUsername(_userManager, _username) {
-        if ((_userManager.getUserTab().find(item => item.getUsername() === _username)) !== undefined)
-            return false;
-        return true;
-    }
-
-    static isUniqueEmail(_userManager, _email) {
-        if ((_userManager.getUserTab().find(item => item.getEmail() === _email)) !== undefined)
-            return false;
-        return true;
     }
 }
 
